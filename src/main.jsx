1988c89fbdf03b73199e9e6c0693dab898ca32ab
vijay brat pandey <vijaybratpandey.com>
2024-02-09 21:20:39 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+export default {
+  build: {
+    rollupOptions: {
+      external: ['/src/main.jsx']
+    }
+  }
+}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
